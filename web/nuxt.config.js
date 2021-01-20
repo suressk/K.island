6d48d760c265cc8bb9c2fa7e8d775b287d61785f
ssk <865801275@qq.com>
2021-01-21 00:19:50 +0800

    fix:相关页面 title 更名

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'nuxt-ssr',
+    title: 'K.island',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
--- web/pages/about.vue
+++ web/pages/about.vue
@@ -11,7 +11,12 @@
 
 <script>
 export default {
-  name: 'About'
+  name: 'About',
+  head () {
+    return {
+      title: "Hi!I'm K. | K.island"
+    }
+  }
 }
 </script>
 
