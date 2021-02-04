e954646f6734a82f4b2c783fbea00c696cc229c2
songkun <songkun@infosouth.com.cn>
2021-02-04 18:52:17 +0800

    fix:delete web/utils/index.d.ts file

--- web/utils/index.d.ts
+++
@@ -1,7 +0,0 @@
-// export interface NotifyOptions {
-//     type: 'success' | 'warning' | 'info' | 'error';
-//     title: string;
-//     message: string;
-// }
-
-export { createNotify, NotifyOptions } from './notify'
