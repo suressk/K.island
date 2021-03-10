dae006b30c40ce81bad0a20eb14f9025369c94f9
K <51023715+suerssk@users.noreply.github.com>
2021-03-10 14:20:43 +0800

    Delete web/components/RainEffect directory

--- web/components/RainEffect/createCanvas.js
+++
@@ -1,9 +0,0 @@
-/**
- * 创建 canvas 元素
- */
-export default function createCanvas(width, height) {
-    const canvas = document.createElement("canvas");
-    canvas.width = width;
-    canvas.height = height;
-    return canvas;
-}
